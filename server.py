fb972bbbddf260f0093fd6417ca85db617745e7d
Quang Dang <42106752+Homuru@users.noreply.github.com>
2019-08-13 22:38:26 +0700

    blah

---
+++ server.py
@@ -0,0 +1,24 @@
+import eventlet
+import socketio
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.on('msg')
+def message(sid, data):
+    print('message ', data)
+
+@sio.on('data')
+def data(sid, data):
+    print('Data: ', data)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
