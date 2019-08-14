2053136a56c975383dafd256eaf939df1985b863
Quang Dang <42106752+Homuru@users.noreply.github.com>
2019-08-14 12:24:00 +0700

    blah

--- server.py
+++ server.py
@@ -6,8 +6,7 @@ import cv2
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
-
-all_image = []
+all_images = []
 
 @sio.on('connect')
 def connect(sid, environ):
@@ -19,11 +18,17 @@ def message(sid, data):
 
 @sio.on('data')
 def data(sid, data):
+    # print('Data: ', data)
     imgdata = base64.b64decode(data)
+    # filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+    # with open(filename, 'wb') as f:
+    #     f.write(imgdata)
+    # img = cv2.imread('some_image.jpg')
+    # print('1: ', img)
     img_np = np.fromstring(imgdata, np.uint8)
     src = cv2.imdecode(img_np, cv2.IMREAD_ANYCOLOR)
-    all_image.append(src)
-    print(len(all_image))
+    all_images.append(src)
+    # print('2: ', src)
 
 
 @sio.on('disconnect')
