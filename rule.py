319196464c0a4e67e4b173c72d68f5f1c6a4ed4b
Tien <viettienha98@gmail.com>
2019-08-15 02:15:52 +0700

    Ok

--- rule.py
+++ rule.py
@@ -81,7 +81,7 @@ def sub_check_05(pose):
         cos = compute_cos_angle(hip, knee, ankle)
         if cos >0.7 and knee[1] < hip[1]:
             return False
-        elif knee[1] < hip[1]+0.02:
+        elif knee[1] < hip[1]+0.08 and knee[1] > hip[1]-0.08:
             return True
 
 def check_05(all_poses,pose):
@@ -94,7 +94,7 @@ def check_05(all_poses,pose):
         print("===============cos",cos,knee[1] ,'hip: ', hip[1],'knee' , knee[1])
         if cos >0.7 and knee[1] < hip[1]:
             return {'has_error':True,'finish':False, 'where': 'knee'}
-        elif knee[1] < hip[1]+0.02 and knee[1] > hip[1]-0.02 and len(all_poses)>3 and sub_check_05(all_poses[-1]) and sub_check_05(all_poses[-2]):
+        elif knee[1] < hip[1]+0.08 and knee[1] > hip[1]-0.08 and len(all_poses)>3 and sub_check_05(all_poses[-1]) and sub_check_05(all_poses[-2]):
 
             return {'has_error':False,'finish':True, 'where': None}
         return {'has_error':False,'finish':False, 'where': None}
